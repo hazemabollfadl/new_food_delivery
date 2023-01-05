0752e33013b94ea50194618536ba40db38a1d77e
eyadamir <eyad1909848@miuegypt.edu.eg>
2023-01-05 21:15:43 +0200

    Home page UI

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -14,9 +14,153 @@ class HomePage extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: Center(
-        child: Text("HomePage"),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: Material(
+              elevation: 7,
+              shadowColor: Colors.grey[300],
+              child: TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  fillColor: Colors.white,
+                  hintText: "Search your product",
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Text(
+              "categories ",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                categories(
+                  categoryName: "Food",
+                  image: "images/logo.jpg",
+                ),
+                categories(
+                  categoryName: "burger",
+                  image: "images/logo.jpg",
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Text(
+              "Products ",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SingleProduct(),
+                SingleProduct(),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Text(
+              "Best Sell ",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SingleProduct(),
+                SingleProduct(),
+              ],
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class categories extends StatelessWidget {
+  final String image;
+  final String categoryName;
+
+  const categories({Key? key, required this.categoryName, required this.image})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(12.0),
+      height: 100,
+      width: 150,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+            image,
+          ),
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Text(categoryName),
+      ),
+    );
+  }
+}
+
+class SingleProduct extends StatelessWidget {
+  const SingleProduct({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.all(12.0),
+          height: 100,
+          width: 150,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Text(
+              "\$30",
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+              ),
+            ))
+      ],
     );
   }
 }
