5cae3e68514bf858226742fd8e0d22c8d62bfc73
Omarashry99 <omar1909833@miuegypt.edu.eg>
2023-01-06 17:37:06 +0200

    Profile page updated+firebase authentication

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,11 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:new_food_delivery/pages/home/home_page.dart';
+import 'package:new_food_delivery/route/routing_page.dart';
+import 'package:new_food_delivery/widgets/my_button.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
-    Widget textFromField({required String hintText}) {
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key?key}):super(key:key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool isEdit = false;
+
+  TextEditingController fullName =
+      TextEditingController(text: userModel.fullName);
+  TextEditingController emailAddress =
+      TextEditingController(text: userModel.emailAddress);
+
+  Widget textFromField({required String hintText}) {
     return Container(
       height: 50,
       width: double.infinity,
@@ -18,47 +33,125 @@ class ProfilePage extends StatelessWidget {
       ),
     );
   }
+
+
+  Widget nonEditTextField() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage("images/non_profile.jpg"),
+              radius: 50,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        textFromField(hintText: userModel.fullName),
+        SizedBox(
+          height: 10,
+        ),
+        textFromField(hintText: userModel.emailAddress),
+      ],
+    );
+  }
+
+  Widget editTextField() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage("images/non_profile.jpg"),
+              radius: 50,
+            ),
+          ],
+        ),
+        TextFormField(
+          controller: fullName,
+          decoration: InputDecoration(hintText: "fullName"),
+        ),
+        TextFormField(
+          controller: emailAddress,
+          decoration: InputDecoration(
+            hintText: "emailAddres",
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+       
+      ],
+    );
+  }
+
+  Future buildUpdateProfile() async {
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .update(
+      {
+        "fullName": fullName.text,
+        "emailAdress": emailAddress.text,
+      },
+    ).then(
+      (value) => RoutingPage.goTonext(
+        context: context,
+        navigateTo: HomePage(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
+        leading: isEdit
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    isEdit = false;
+                  });
+                },
+                icon: Icon(Icons.close),
+              )
+            : IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back,
+                ),
+              ),
         actions: [
-          IconButton(onPressed: (){ }, icon: Icon(Icons.edit),
-          ),  
+          IconButton(
+            onPressed: () {
+              setState(() {
+                isEdit = true;
+              });
+            },
+            icon: Icon(Icons.edit),
+          ),
         ],
-        
       ),
       body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Expanded(child: Column( 
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-  Row( 
-               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage("images/non_profile.jpg"),
-                             radius:50,
-                ),
-              ],
-            ),
-           textFromField(hintText: userModel.fullName),
-           textFromField(hintText: userModel.emailAddress),
-              ],
-            ),
-            ),
-            Expanded(child: Container(),
-            )
-          ],
-
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: isEdit ? editTextField() : nonEditTextField(),
+              ),
+            ],
+          ),
         ),
       ),
-      )
     );
   }
 }
